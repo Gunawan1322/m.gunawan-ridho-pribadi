90ccf45d1455fea55277a7ae4a091642343126bf
Gunawan1322 <80387398+Gunawan1322@users.noreply.github.com>
2021-03-18 15:37:01 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,16 @@
+#menghitung berapa hati mr bekicot bisa naik gedung 100 meter
+S=5
+print("keniakan siang hari 5 meter")
+M=4
+print ("merosot malam hari 4 meter")
+g=100
+#mencari kenaikan perhari 
+print("tinggi gedung 100 meter")
+hasil=S-M
+#berapa hari mr.bekicot sampai keatas gedung 
+print("kenaikan perhari :",hasil,"meter" )
+akhir=100*hasil
+print("jadi mr.bekicot butuh",akhir ," hari untuk naik kepuncak gedung")
+
+
+
